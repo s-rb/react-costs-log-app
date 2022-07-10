581dd6ce54a650ff7d8adc5bcfc422716ba639e9
Roman Surkov <s.rb@list.ru>
2022-07-10 19:11:42 +0300

    Форма показывается и скрывается по нажатию кнопок

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -80,6 +80,7 @@ const CostForm = (props) => {
             </div>
             <div className='new-cost__actions'>
                 <button type='submit'>Добавить расход</button>
+                <button type='button' onClick={props.onCancel}>Отмена</button>
             </div>
         </div>
     </form>
--- src/components/NewCost/NewCost.js
+++ src/components/NewCost/NewCost.js
@@ -1,14 +1,28 @@
 import CostForm from "./CostForm";
 import './NewCost.css';
+import {useState} from "react";
 
 const NewCost = (props) => {
 
+    const [isFormVisible, setIsFormVisible] = useState(false)
+
     const saveCostDataHandler = (inputCostData) => {
-        props.onAddCost(inputCostData);
+        let costData = {...inputCostData, id: Math.random().toString()};
+        props.onAddCost(costData);
+        setIsFormVisible(false);
+    }
+
+    const inputCostDataHandler = () => {
+        setIsFormVisible(true);
+    }
+
+    const cancelCostHandler = () => {
+        setIsFormVisible(false);
     }
 
     return <div className='new-cost'>
-        <CostForm onSaveCostData={saveCostDataHandler}/>
+        {!isFormVisible && <button onClick={inputCostDataHandler}>Добавить новый расход</button>}
+        {isFormVisible && <CostForm onSaveCostData={saveCostDataHandler} onCancel={cancelCostHandler}/>}
     </div>
 }
 
