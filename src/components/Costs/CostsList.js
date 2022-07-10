b82bd5b586e980d5782a6b745276f08b1723eb11
Roman Surkov <s.rb@list.ru>
2022-07-10 15:48:37 +0300

    Отредактирован CostsList

--- src/components/Costs/CostsList.js
+++ src/components/Costs/CostsList.js
@@ -2,22 +2,20 @@ import "./CostsList.css";
 import CostItem from "./CostItem";
 
 const CostsList = (props) => {
-    let costsContent = props.filteredElements.length === 0 && <p>В этом году нет расходов</p>;
-    if (props.filteredElements.length > 0) {
-        costsContent = props.filteredElements.map(cost => (<CostItem
-            key={cost.id}
-            date={cost.date}
-            description={cost.description}
-            amount={cost.price}
-        />))
+    if (props.filteredElements.length === 0) {
+        return <h2 className="cost-list__fallback">В этом году расходов нет</h2>
     }
 
     return (
-        // <ul className="cost-list">
-        //
-        // </ul>
-        {costsContent}
+        <ul className="cost-list">
+            {props.filteredElements.map(cost => (<CostItem
+                key={cost.id}
+                date={cost.date}
+                description={cost.description}
+                amount={cost.price}
+            />))}
+        </ul>
     )
-}
+};
 
 export default CostsList;
